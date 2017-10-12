215297fd2b8fcebcc9d973d9dedcd11427e49da4
Anuj Bhai Mehta <mehta.anuj2309@gmail.com>
2017-10-12 23:30:16 +0530

    data visualization done

---
+++ script.m
@@ -0,0 +1,16 @@
+%% Initialization
+clear;
+input_layer_size  = 400;  				% 20x20 Input Images of Digits
+hidden_layer_size = 25;   				% 25 hidden units
+num_labels = 10;          				% 10 labels, from 1 to 10   
+
+%% Load and visualize data
+
+load('data_train.mat'); 				% training data stored in arrays X, y
+m = size(X, 1);
+
+rand_indices = randperm(m); 			% Randomly select 100 data points to display
+selected = X(rand_indices(1:100), :);
+displayData(selected);
+
+
